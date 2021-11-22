cf80950ef1271876275b8a381e34cb01b527a5ab
bailong-huang <61515633+Bailong-Huang@users.noreply.github.com>
2021-11-21 21:20:39 -0600

    Update TestServlet.java
    Erase the conflict part

--- HOMII/src/com/comp/controller/TestServlet.java
+++ HOMII/src/com/comp/controller/TestServlet.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.comp.controller;
 
 import java.io.IOException;
@@ -28,34 +27,3 @@ public class TestServlet extends HttpServlet {
 	}
 
 }
-=======
-package com.comp.controller;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/TestServlet")
-public class TestServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-    public TestServlet() {
-        super();
-
-    }
-
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
-		doPost(req, res);
-	}
-
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
-
-	}
-
-}
->>>>>>> main
