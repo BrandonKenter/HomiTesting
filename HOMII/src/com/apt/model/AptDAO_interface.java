5d8653814cbde8318e6bacf19f7ce3b01f865e9b
bailong-huang <61515633+Bailong-Huang@users.noreply.github.com>
2021-11-21 21:18:47 -0600

    Update AptDAO_interface.java
    Erase the conflict

--- HOMII/src/com/apt/model/AptDAO_interface.java
+++ HOMII/src/com/apt/model/AptDAO_interface.java
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-package com.apt.model;
-
-import java.util.List;
-
-public interface AptDAO_interface {
-	public AptVO findByPrimaryKey(Integer apt_no);
-	public AptVO findByPrimaryKeyByApName(String ap_name);
-	public AptVO getOnePicByApName (String ap_name);
-	public int insert(AptVO aptVO);
-	public List<AptVO> getAllByMemNo(Integer member_no);
-}
-=======
 package com.apt.model;
 
 import java.util.List;
@@ -24,4 +11,3 @@ public interface AptDAO_interface {
 	public List<AptVO> getAllDesc();
 	public List<AptVO> getAllAsc();
 }
->>>>>>> main
