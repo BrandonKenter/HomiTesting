1d8e571dadbcedeb6225a8d32423f0efcb49deb3
bailong-huang <61515633+Bailong-Huang@users.noreply.github.com>
2021-11-21 21:25:54 -0600

    Update RegDAO_interface.java
    Erase the conflict part

--- HOMII/src/com/reg/model/RegDAO_interface.java
+++ HOMII/src/com/reg/model/RegDAO_interface.java
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-package com.reg.model;
-
-import java.util.List;
-
-public interface RegDAO_interface {
-	public int insert(RegVO regVO);
-	public RegVO findByPrimaryKey(Integer member_no);
-	public RegVO findByPrimaryKeyByRegNo(Integer reg_no);
-	public List<RegVO> getAllByMemNo(Integer member_no);
-	public List<RegVO> getAllByLandName(String land_name);
-	public int updateRes(RegVO regVO);
-	public List<RegVO> getAllByLandNameWithApproval(String land_name);
-	public List<RegVO> getAllByApName(String ap_name);
-}
-=======
 package com.reg.model;
 
 import java.util.List;
@@ -28,4 +12,4 @@ public interface RegDAO_interface {
 	public List<RegVO> getAllByLandNameWithApproval(String land_name);
 	public List<RegVO> getAllByApName(String ap_name);
 }
->>>>>>> main
+
